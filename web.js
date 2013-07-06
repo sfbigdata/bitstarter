25cfa1adf0e8cb721cf160329d39e964169076f2
Ubuntu <ubuntu@ip-10-164-74-32.ec2.internal>
2013-07-06 18:19:23 +0000

    fix error

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var data_content = '';	
+  var data = fs.readFileSync('index.html');
+  response.send(data.toString());  
+  
 });
 
 var port = process.env.PORT || 5000;
